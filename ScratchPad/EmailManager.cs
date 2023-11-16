e4f60ab8dcdeec0ca92d87a1deeaa32ae4dc4b78
Baron Stubblefield <Baron.Stubblefield@kmob.com>
2023-11-15 20:30:32 -0800

    added scratchpad project

---
+++ ScratchPad/EmailManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using MsgKit;
+
+namespace ScratchPad;
+
+public class EmailManager
+{
+    static void RemoveAttachment(string emailFilePath, string attachmentName)
+    {
+        try
+        {
+            using var msg = new MsgReader.Outlook.Storage.Message(emailFilePath);
+            RemoveAttachmentFromEmail(msg, attachmentName);
+            //msg.Save();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    static void RemoveAttachmentFromEmail(MsgReader.Outlook.Storage.Message msg, string attachmentName)
+    {
+        // Find the attachment by name
+        //var attachmentToRemove = msg.Attachments.FirstOrDefault(a => a.FileName == attachmentName);
+        var attachmentToRemove = msg.Attachments.FirstOrDefault(a => a.ToString() == attachmentName);
+
+        // Remove the attachment
+        if (attachmentToRemove != null)
+        {
+            msg.Attachments.Remove(attachmentToRemove);
+        }
+    }
+}
---
+++ ScratchPad/Program.cs
@@ -0,0 +1,17 @@
+﻿
+RunEmailManager();
+
+void RunEmailManager()
+{
+    var mgr = new EmailAttachmentManager();
+    mgr.SendEmail("Hello, World!");
+}
+
+class EmailAttachmentManager
+{
+    public void SendEmail(string message)
+    {
+        Console.WriteLine($"Sending email: {message}");
+    }
+}
+
